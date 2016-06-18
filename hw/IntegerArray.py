b6cc2a3c18c8bd9ebf32b6fc6260823a8c2a0022
keguoh <timmy0703@gmail.com>
2016-06-18 17:29:50 -0500

    update

--- hw/IntegerArray.py
+++ hw/IntegerArray.py
@@ -1,25 +1,44 @@
-def inversion_count(alist):
+def inversion_count(alist, count):
     if len(alist) > 1:
-        print 'spliting' + alist
+        #print 'spliting %s' % alist
         mid = len(alist)//2
         first_half = alist[:mid]
         second_half = alist[mid:]
 
-        inversion_count(first_half)
-        inversion_count(second_half)
+        inversion_count(first_half, count)
+        inversion_count(second_half, count)
 
         i = 0
         j = 0
         k = 0
 
         while len(first_half) > i and len(second_half) > j:
-            if first_half[i] < second_half[j]:
+            if first_half[i] <= second_half[j]:
                 alist[k] = first_half[i]
-                i++
+                i = i + 1
             else:
                 alist[k] = second_half[j]
+                count += len(first_half) - i
+                print "Now we have %s inversions" % count
+                j = j + 1
+            k = k + 1
 
-                
+        while len(first_half) > i:
+            alist[k] = first_half[i]
+            i = i + 1
+            k = k + 1
 
-    else:
-        print 'reached single-item list'
+        while len(second_half) > j:
+            alist[k] = second_half[j]
+            j = j + 1
+            k = k + 1
+        print (alist, ' merged')
+    #else:
+        #print 'single-item list %s reached' % alist
+
+#A = [1,3,5,2,4,6]
+A = [1,5,3,2,4]
+print A
+count = 0
+inversion_count(A, count)
+print 'list becomes %s and count is %s' % (A, count)
