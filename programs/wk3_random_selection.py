d63ca6836d5a0eb7bf57702131a6bb403facf4ca
keguoh <timmy0703@gmail.com>
2016-07-01 00:13:14 -0500

    random selection

--- programs/wk3_random_selection.py
+++ programs/wk3_random_selection.py
@@ -1,38 +1,36 @@
 #https://github.com/danishm/python-sorting-algos/blob/master/sorting.py
 
 def rand_seletion(alist, n):
-    if len(alist)
-
-
-def quick_sort(alist):
-    if len(alist) < 2: return alist
+    if len(alist) < n:
+        print "the order is out of range!"
     else:
-        pivot = alist[0]
-        i, j = 1, 1
-        while j < len(alist):
-            if alist[j] < pivot:
-                #print "exchanging %s at %s with %s at %s" % (alist[j], j, alist[i], i)
-                (alist[j], alist[i]) = (alist[i], alist[j])
-                i += 1
-            j += 1
-        (alist[0], alist[i-1]) = (alist[i-1], alist[0])
-        print "getting %s" % alist
+        # if len(alist) < 2: return alist
+        # else:
+            pivot = alist[0]
+            i, j = 1, 1
+            while j < len(alist):
+                if alist[j] < pivot:
+                    #print "exchanging %s at %s with %s at %s" % (alist[j], j, alist[i], i)
+                    (alist[j], alist[i]) = (alist[i], alist[j])
+                    i += 1
+                j += 1
+            (alist[0], alist[i-1]) = (alist[i-1], alist[0])
+            print "the list becomes %s with pivot %s" % (alist, pivot)
+            if i == n: 
+                print "the result is %s at position %s" % (alist[i-1], i)
+                print alist[i-1]
+            elif i < n: 
+                print "choose the later part %s with result at %s" % (alist[i:], i)
+                rand_seletion(alist[i:], n-i)
+            else: 
+                print "choose the front part %s" % alist[:(i-1)]
+                rand_seletion(alist[:(i-1)], n)
 
-        left = alist[:(i-1)]
-        quick_sort(left)
-        alist[:(i-1)] = left
-        
-        right = alist[i:]
-        quick_sort(right)
-        alist[i:] = right
-        print "The list becomes %s" % alist
 
 
 alist = [3, 2, 8, 5, 1, 4, 7, 6]
 # alist = [1, 3, 5, 2, 4, 6]
-quick_sort(alist)
+rand_seletion(alist, 7)
 print alist
-
-
 
 
