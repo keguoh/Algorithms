30c5d20cbfed714ea9c4af0ad3cdf0a9fca5d8d4
keguoh <timmy0703@gmail.com>
2016-07-14 22:57:15 -0500

    update

--- programs/wk4_homework.py
+++ programs/wk4_homework.py
@@ -8,18 +8,140 @@ Output Format: You should output the sizes of the 5 largest SCCs in the given gr
 WARNING: This is the most challenging programming assignment of the course. Because of the size of the graph you may have to manage memory carefully. The best way to do this depends on your programming language and environment, and we strongly suggest that you exchange tips for doing this on the discussion forums.
 """
 
-file = open("wk4_SCC.txt", "r")
-# file = open("wk4_test1.txt", "r")
+
+fin=open("wk4_SCC.txt", "r")
+# fin=open('wk4_test1.txt', 'r')
+# fin=open('wk4_test2.txt', 'r')
+# fin=open('wk4_test3.txt', 'r')
+# fin=open('wk4_test4.txt', 'r')
+
 n_lines = 0
 tail = []
-for line in file:
-	n_lines += 1
-	tail.append(int(line.split()[0]))
+for line in fin:
+    n_lines += 1
+    a = int(line.split()[0])
+    if len(tail) == 0:
+    	tail.append(a)
+    elif  a > tail[len(tail)-1]:
+        tail.append(a)
 print n_lines
-n_nodes = tail[n_lines-1]
-print n_nodes
-# easy to find:
-# 5105043 lines
-#  875714 vertices
+N = tail[len(tail)-1]
+print N
 
-# use iterative
+fin.seek(0)
+
+G = {}
+#graph with the directions as per the input file
+Grev = {}
+#graph with the directions reversed
+for i in range(1, N+1):
+    G[i]=[]
+    Grev[i]=[]
+for line in fin:
+    v1=int(line.split()[0])
+    v2=int(line.split()[1])
+    G[v1].append(v2)
+    Grev[v2].append(v1)
+fin.close()
+
+# print G[N]
+print 'the Grev is %s\n' % Grev
+
+#global variables to keep record
+visited = {}
+#whether the node is visited or not
+finish = {}
+#to keep a record of finishing time of the nodes in the first pass
+leader = {}
+#to keep record of the leaders of the scc in the 2nd pass
+
+
+def dfs(G, i):
+	global t, s
+	stack = []
+	f = []
+	stack.append(i)
+	leader[i] = s
+	while(len(stack) > 0):
+		u = stack[len(stack)-1]
+		if visited[u] == 0:
+			leader[u] = s
+			visited[u] = 1
+			if len(G[u]) == 0:
+				t += 1
+				finish[u] = t
+				stack.pop()
+				print 'zero outdegree vertex %s has been reached and its finish is %s' % (u, t)
+			else:
+				for w in G[u]:
+					if visited[w] == 0 and w not in stack:
+						stack.append(w)
+						print "%s's unvisited neighbor %s has been reached, and stack is %s" % (u, w, stack)
+		else:
+			t += 1
+			finish[u] = t 
+			print 'visited %s has been reached and its finish is %s' % (u, t)
+			stack.pop()
+	visited[i] = 1
+
+
+# i = 2
+# s = i
+# t = 0
+# dfs(Grev, i)
+# print finish
+# print leader
+
+
+def dfs_loop(G):
+    global t, s, N
+    t = 0 #number of nodes processed so far
+    s = 0 #current source vertex
+    i = N
+    for k in range(1, N+1):
+#initially setting all to be none 
+        visited[k] = 0
+        finish[k] = 0
+        leader[k] = 0
+
+    while(i > 0):
+        if(visited[i] == 0):
+            s = i
+            print '\nthe leader is %s' % i
+            dfs(G, i)
+        i -= 1
+
+#everything set
+#finally ready to call the functions with the right graph kind in the corr order
+dfs_loop(Grev) 
+#THE FIRST LOOP ON REVERSE GRAPH
+#done with the second step of the three steps algorithm
+# construct new graph
+print 'the finish after the first pass is %s\n' % finish
+# print 'the leader is %s' % leader
+newGraph = {}
+for i in range(1, N+1):
+    temp=[]
+    for x in G[i]: 
+        temp.append(finish[x])
+#to keep record of the edges of the actual nodes in the correct order
+    newGraph[finish[i]] = temp
+
+print 'the newGraph is %s\n' % newGraph
+
+dfs_loop(newGraph) #THE SECOND LOOP 
+#done with the third step of the three steps algorithm
+
+print 'the leader is '
+    # statistics
+lst = sorted(leader.values())
+stat = []
+pre = 0
+for i in range(0, N-1):
+    if lst[i] != lst[i+1]:
+        stat.append(i+1-pre)
+        pre = i+1
+stat.append(N-pre)
+L = sorted(stat)
+L.reverse()
+print(L[0:10])
--- programs/wk4_other1_homework.py
+++ programs/wk4_other1_homework.py
@@ -1,19 +1,31 @@
-
+#This method uses recursive calls
 
 #!/usr/bin/env python
 import sys
 sys.setrecursionlimit(5000)
 
 
-N = 875714
-G={}
+# fin=open("wk4_SCC.txt", "r")
+fin=open('wk4_test1.txt', 'r')
+
+n_lines = 0
+tail = []
+for line in fin:
+    n_lines += 1
+    tail.append(int(line.split()[0]))
+print n_lines
+N = tail[n_lines-1]
+print N
+
+fin.seek(0)
+
+G = {}
 #graph with the directions as per the input file
-Grev={}
+Grev = {}
 #graph with the directions reversed
 for i in range(1, N+1):
     G[i]=[]
     Grev[i]=[]
-fin=open("wk4_SCC.txt", "r")
 for line in fin:
     v1=int(line.split()[0])
     v2=int(line.split()[1])
@@ -21,41 +33,40 @@ for line in fin:
     Grev[v2].append(v1)
 fin.close()
 #global variables to keep record
-visited={}
+visited = {}
 #whether the node is visited or not
-finish={}
+finish = {}
 #to keep a record of finishing time of the nodes in the first half
-leader={}
+leader = {}
 #to keep record of the leaders of the scc in the 2nd pass
-t = 0
-s = 0
+
 
 def dfs(G, i):
     global t, s
-    visited[i]=1
-    leader[i]=s
+    visited[i] = 1
+    leader[i] = s
     for j in G[i]:
-        if(visited[j]==0): 
-            dfs(G,j)
-    t=t+1
+        if(visited[j] == 0):
+            dfs(G, j)
+    t += 1
     finish[i]=t
 
 def dfs_loop(G):
-    global t, s,N
-    t=0 #number of nodes processed so far
-    s=0 #current source vertex
-    i=N
-    for i in reversed(range(1,N+1)):                #ADDED BY KEGUO
+    global t, s, N
+    t = 0 #number of nodes processed so far
+    s = 0 #current source vertex
+    i = N
+    for i in range(1, N+1):               #ADDED BY KEGUO
 #initially setting all to be none 
-        visited[i]=0
-        finish[i]=0
-        leader[i]=0
+        visited[i] = 0
+        finish[i] = 0
+        leader[i] = 0
 
-    while(i>0):
-        if(visited[i]==0):
-            s=i
-            dfs(G,i)
-        i=i-1
+    while(i > 0):
+        if(visited[i] == 0):
+            s = i
+            dfs(G, i)
+        i -= 1
 
 
 #everything set
@@ -64,8 +75,8 @@ dfs_loop(Grev)
 #THE FIRST LOOP ON REVERSE GRAPH
 #done with the second step of the three steps algorithm
 # construct new graph
-newGraph={}
-for i in range(1,N+1):
+newGraph = {}
+for i in range(1, N+1):
     temp=[]
     for x in G[i]: 
         temp.append(finish[x])
@@ -76,11 +87,11 @@ dfs_loop(newGraph) #THE SECOND LOOP
 #done with the third step of the three steps algorithm
 
     # statistics
-lst= sorted(leader.values())
-stat=[]
-pre=0
-for i in range(0,N-1):
-    if lst[i]!=lst[i+1]:
+lst = sorted(leader.values())
+stat = []
+pre = 0
+for i in range(0, N-1):
+    if lst[i] != lst[i+1]:
         stat.append(i+1-pre)
         pre=i+1
 stat.append(N-pre)
