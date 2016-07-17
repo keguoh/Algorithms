20819508eb87cbc04a93c0ffadb2a081eb21d1c0
keguoh <timmy0703@gmail.com>
2016-07-16 22:55:54 -0500

    hw5-program

--- programs/wk5_dijkstra.py
+++ programs/wk5_dijkstra.py
@@ -21,13 +21,48 @@ file = open('wk5_dijkstraData.txt', 'r')
 G = {}
 for line in file:
 	node = int(line.split()[0])
-	edge = int(line.split()[1:])
-	G[node] = edge
-
-# def dij(G, s):
-# 	X = [s]
-# 	A = {s:0}
-# 	B = {s:[]}
-# 	while len(X) < len(G):
+	edge_dis = []
+	for edges in line.split()[1:]:
+		edge_dis.append({int(edges.split(',')[0]): int(edges.split(',')[1])})
+	G[node] = edge_dis
 
 
+def Dijk(G, s):
+	global X, A, B
+	X = [s]
+	A = {s:0}
+	B = {s:[]}
+	i = 1
+	while len(X) < len(G) and i<203:
+		dis_v = {}
+		# print 'the X is %s when i = %s' % (X, i)
+		for v in X:
+			node_w = []
+			dis_w = []
+			for w in G[v]:
+				if w.keys()[0] not in X:
+					node_w.append(w.keys()[0])
+					dis_w.append(A[v] + w.values()[0])
+			if len(dis_w) > 0:
+				min_dis_w = min(dis_w)
+				dis_v[(v, node_w[dis_w.index(min_dis_w)])] = min_dis_w
+		if len(dis_v) > 0:
+			w_star = min(dis_v, key=dis_v.get)[1]
+			v_star = min(dis_v, key=dis_v.get)[0]
+			# print 'the v_star is %s, and w_star is %s when i = %s\n' % (v_star, w_star, i)
+			min_dis = dis_v[(v_star, w_star)]
+			X.append(w_star)
+			A[w_star] = min_dis
+			# print 'B before is %s' % B
+			B[w_star] = list(B[v_star])
+			B[w_star].append(v_star)
+			# print 'B after is %s' % B
+			i += 1
+
+time1 = time.time()
+Dijk(G, 1)
+
+
+for x in [7,37,59,82,99,115,133,165,188,197]:
+	print A[x]
+print 'the process takes %s mins' % ((time.time()-time1)/60)
